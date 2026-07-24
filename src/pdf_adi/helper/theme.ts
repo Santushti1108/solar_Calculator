0fe3ce6ee00436c3e63ef31b2666ed40420fdc6a
Aditya <adiroychaudhuri@gmail.com>
2026-07-24 15:46:37 +0530

    theme for the pdf

---
+++ src/pdf_adi/helper/theme.ts
@@ -0,0 +1,21 @@
+export const COLORS = {
+  primary: [249, 168, 37],      // Solar yellow
+  secondary: [30, 41, 59],      // Slate
+  white: [255, 255, 255],
+  black: [33, 33, 33],
+  gray: [120, 120, 120],
+  light: [245, 245, 245]
+}
+
+export const SPACING = {
+  margin: 20,
+  sectionGap: 12,
+  cardPadding: 10
+}
+
+export const FONT = {
+  title: 24,
+  heading: 18,
+  body: 12,
+  small: 10
+}
