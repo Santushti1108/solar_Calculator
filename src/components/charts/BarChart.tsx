70658ae406859e7a28cae158537f3c55d7f74c04
Aditya <adiroychaudhuri@gmail.com>
2026-07-24 15:42:47 +0530

    updated both bar and line chart components for forwarding its reference to the parent component. Also added toBase64Image function.

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -1,8 +1,24 @@
 import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Tooltip } from 'chart.js';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { Bar } from 'react-chartjs-2';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
-export function BarChart({ data, options }: { data: object; options: object }) {
-  return <Bar data={data as never} options={options as never} />;
-}
+export type BarChartRef = {
+  toBase64Image: () => string;
+};
+
+export const BarChart = forwardRef<BarChartRef, { data: object, options: object }>(
+  function LineChart({ data, options }, ref) {
+    const chartRef = useRef<ChartJS<"bar"> | null>(null);
+
+    useImperativeHandle(ref, () => ({
+      toBase64Image: () => {
+        const image = chartRef.current?.toBase64Image() ?? "";
+        return image;
+      },
+    }));
+
+    return <Bar ref={chartRef} data={data as never} options={options as never}/>
+  }
+)
--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -8,10 +8,27 @@ import {
   PointElement,
   Tooltip,
 } from 'chart.js';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { Line } from 'react-chartjs-2';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, Tooltip, Legend);
 
-export function LineChart({ data, options }: { data: object; options: object }) {
-  return <Line data={data as never} options={options as never} />;
-}
+export type LineChartRef = {
+  toBase64Image: () => string;
+};
+
+
+export const LineChart = forwardRef<LineChartRef, { data: object, options: object }>(
+  function LineChart({ data, options }, ref) {
+    const chartRef = useRef<ChartJS<"line"> | null>(null);
+
+    useImperativeHandle(ref, () => ({
+      toBase64Image: () => {
+        const image = chartRef.current?.toBase64Image() ?? "";
+        return image;
+      },
+    }));
+
+    return <Line ref={chartRef} data={data as never} options={options as never}/>
+  }
+)
